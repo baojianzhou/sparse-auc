73d0958834b7b93b938e03c3369ef2f4c64bed98
Baojian Zhou <bzhou6@albany.edu>
2019-09-02 19:57:14 -0400

    update hw 1

---
+++ slurm_run_solam.py
@@ -0,0 +1,7 @@
+#!bin/bash
+
+import multiprocessing
+import sys
+import os
+
+print(sys.argv[1], sys.argv[2])
--- slurm_run_solam.sh
+++ slurm_run_solam.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-#SBATCH -p batch # We can choose batch,ceashpc,kratos,minerva,snow
-#SBATCH --cpus-per-task=30
-#SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=2000mb
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=bzhou6@albany.edu
-#SBATCH -o /network/rit/home/bz383376/slurm-%j.out
+#SBATCH --job-name=slurm-run-sparse
+#SBATCH --output=logs/array_%A_%a.out
+#SBATCH --array=0-99
+#SBATCH --time=01:00:00
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+
+# the environment variable SLURM_ARRAY_TASK_ID contains
+# the index corresponding to the current job step
+PYTHON="/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python"
+PYTHON slurm_run_solam.py $SLURM_ARRAY_TASK_ID 100
