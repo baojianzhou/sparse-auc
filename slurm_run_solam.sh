cd09a25e07f7f2ae278f35007cea72fd7b1cb9d7
Baojian Zhou <bzhou6@albany.edu>
2019-09-03 06:53:54 -0400

    update sh

--- slurm_run_solam.sh
+++ slurm_run_solam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=slurm-run-sparse
 #SBATCH --output=array_%A_%a.out
-#SBATCH --array=0-2
-#SBATCH --time=01:00:00
-#SBATCH --partition=ceashpc
+#SBATCH --array=0-100
+#SBATCH --time=12:00:00
+#SBATCH --partition=batch
 #SBATCH --ntasks=1
 
 # the environment variable SLURM_ARRAY_TASK_ID contains
