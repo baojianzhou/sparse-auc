9db53816d37e3550b73a596919b0c40820e8fc5d
Baojian Zhou <bzhou6@albany.edu>
2019-09-05 09:30:37 -0400

    dd

--- remove_results.py
+++ remove_results.py
@@ -7,5 +7,5 @@ import time
 data_path = '/network/rit/lab/ceashpc/bz383376/data/icml2020/09_sector/lib-svm-data/'
 
 for i in range(1000):
-    if os.path.exists(data_path + 'model_select_%04d.pkl'):
-        os.remove(data_path + 'model_select_%04d.pkl')
+    if os.path.exists(data_path + 'model_select_%04d.pkl' % i):
+        os.remove(data_path + 'model_select_%04d.pkl' % i)
