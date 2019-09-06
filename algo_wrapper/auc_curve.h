1dd86d5a59a8eded889dd0c7715d57eb8b2981c5
Baojian Zhou <bzhou6@albany.edu>
2019-09-06 11:11:56 -0400

    test slurm

---
+++ algo_wrapper/auc_curve.h
@@ -0,0 +1,8 @@
+//
+// Created by baojian on 9/6/19.
+//
+
+#ifndef SPARSE_AUC_AUC_CURVE_H
+#define SPARSE_AUC_AUC_CURVE_H
+
+#endif //SPARSE_AUC_AUC_CURVE_H
---
+++ test_auc_curve.c
@@ -0,0 +1,17 @@
+//
+// Created by baojian on 9/6/19.
+//
+
+#include <stdbool.h>
+#include "auc_curve.h"
+
+// y = np.array([1, 1, 2, 2])
+void test_auc_curve() {
+    double y[] = {1, 1, -1, -1};
+    double scores = {0.1, 0.4, 0.35, 0.8};
+}
+
+
+int main() {
+    return 0;
+}
