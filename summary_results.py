a9f29af948230ea7edb0189d41da3ae87131db50
Baojian Zhou <bzhou6@albany.edu>
2019-09-11 10:18:08 -0400

    dd

--- summary_results.py
+++ summary_results.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
-import paramiko
-from scp import SCPClient
+import numpy as np
 import pickle as pkl
+from itertools import product
+
+data_path = '/network/rit/lab/ceashpc/bz383376/data/icml2020/02_usps/'
 
 
-def createSSHClient(server, port, user, password):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    return client
-
-
-def get_files():
-    ssh = createSSHClient("headnode7.rit.albany.edu", "22", "bz383376", "@198957A1b2c3d4")
-    scp = SCPClient(ssh.get_transport())
-    scp.get(r'/network/rit/lab/ceashpc/bz383376/data/icml2020/02_usps/ms_spam_l2_*',
-            r'/network/rit/lab/ceashpc/bz383376/data/icml2020/02_usps/')
-
-
-def test():
-    data_path = "/network/rit/lab/ceashpc/bz383376/data/icml2020/02_sector/"
-    id_address = "scp -r bz383376@headnode7.rit.albany.edu:/network/rit/lab/ceashpc/bz383376/data/icml2020/02_sector/ms_spam_l2_* /network/rit/lab/ceashpc/bz383376/data/icml2020/02_sector/"
-    os.system("scp -r " + id_address + folder + dest)
-
-    print('test')
-
-
-def result_summary():
-    data_path = "/network/rit/lab/ceashpc/bz383376/data/icml2020/02_usps/"
+def result_summary(num_passes):
+    num_runs, k_fold = 5, 5
+    all_para_space = []
+    for run_id, fold_id in product(range(num_runs), range(k_fold)):
+        for para_xi in 10. ** np.arange(-7, -2., 0.5, dtype=float):
+            for para_beta in 10. ** np.arange(-6, 1, 1, dtype=float):
+                para_row = (run_id, fold_id, para_xi, para_beta, num_passes, num_runs, k_fold)
+                all_para_space.append(para_row)
+    # only run sub-tasks for parallel
+    num_sub_tasks = len(all_para_space) / 100
     all_results = []
-    for task_id in range(100):
-        task_start, task_end = int(task_id) * 33, int(task_id) * 33 + 33
-        f_name = data_path + 'ms_spam_l2_%04d_%04d_%04d.pkl' % (task_start, task_end, 50)
+    for i in range(100):
+        task_start, task_end = int(i) * num_sub_tasks, int(i) * num_sub_tasks + num_sub_tasks
+        f_name = data_path + 'ms_spam_l2_%04d_%04d_%04d.pkl' % (task_start, task_end, num_passes)
         results = pkl.load(open(f_name, 'rb'))
         all_results.extend(results)
-    pkl.dump(all_results, open(data_path + 'ms_spam_l2_0000_3000_50.pkl', 'wb'))
+    pkl.dump(all_results, open(data_path + 'ms_spam_l2_passes_%04d.pkl' % num_passes, 'wb'))
 
 
-result_summary()
+for i in [1, 5, 10, 20, 30, 50]:
+    result_summary(i)
