e1eb26efbac8e23660d130e1bb95c085c40d9de9
Baojian Zhou <bzhou6@albany.edu>
2019-09-11 08:46:51 -0400

    dd

--- slurm_run.sh
+++ slurm_run.sh
@@ -6,13 +6,13 @@
 # $5: python_file
 #SBATCH --job-name=sparse-run
 #SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/sparse-auc/logs/array_%A_%a.out
-#SBATCH --array=$1-$2
-#SBATCH --nodelist=$3-$4
+#SBATCH --array=81-99
+#SBATCH --nodelist=ceashpc-08
 #SBATCH --time=12:00:00
-#SBATCH --partition=$3
+#SBATCH --partition=ceashpc
 #SBATCH --mem=2G
 #SBATCH --ntasks=1
 # %A_%a notation is filled in with the master job id (%A) and the array task id (%a)
 # the environment variable SLURM_ARRAY_TASK_ID contains
 # the index corresponding to the current job step
-/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python $5
+/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_02_usps.py 
