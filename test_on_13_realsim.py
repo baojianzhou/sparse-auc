38572523eb3154c76184f936c1b72b9a4dc0aac4
Baojian Zhou <bzhou6@albany.edu>
2019-09-11 20:22:46 -0400

    dd

--- test_on_13_realsim.py
+++ test_on_13_realsim.py
@@ -82,7 +82,7 @@ def get_sub_data_by_indices(data, tr_index, sub_tr_ind):
     return sub_x_values, sub_x_indices, sub_x_positions, sub_x_len_list
 
 
-def test_single_ms_spam_l2(para):
+def run_single_ms_spam_l2(para):
     """
     Model selection of SPAM-l2
     :return:
@@ -157,7 +157,7 @@ def test_single_ms_spam_l2(para):
             'run_time': run_time}
 
 
-def run_model_selection_spam_l2():
+def run_ms_spam_l2():
     if 'SLURM_ARRAY_TASK_ID' in os.environ:
         task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
     else:
@@ -177,13 +177,163 @@ def run_model_selection_spam_l2():
     list_tasks = all_para_space[task_start:task_end]
     list_results = []
     for task_para in list_tasks:
-        result = test_single_ms_spam_l2(task_para)
+        result = run_single_ms_spam_l2(task_para)
         list_results.append(result)
     file_name = 'ms_spam_l2_task_%02d_passes_%03d.pkl' % (task_id, global_passes)
     pkl.dump(list_results, open(os.path.join(root_path, file_name), 'wb'))
 
 
-def run_model_selection_sht_am():
+def run_single_ms_spam_l2(para):
+    """
+    Model selection of SPAM-l2
+    :return:
+    """
+    run_id, fold_id, para_xi, para_beta, num_passes, num_runs, k_fold = para
+    s_time = time.time()
+    data = load_dataset(root_path=root_path, name='realsim')
+    para_spaces = {'conf_num_runs': num_runs,
+                   'conf_k_fold': k_fold,
+                   'para_num_passes': num_passes,
+                   'para_beta': para_beta,
+                   'para_l1_reg': 0.0,  # no l1 regularization needed.
+                   'para_xi': para_xi,
+                   'para_fold_id': fold_id,
+                   'para_run_id': run_id,
+                   'para_verbose': 0,
+                   'para_is_sparse': 1,
+                   'para_step_len': 400000000,
+                   'para_reg_opt': 1,
+                   'data_id': 13,
+                   'data_name': '13_realsim'}
+    tr_index = data['run_%d_fold_%d' % (run_id, fold_id)]['tr_index']
+    te_index = data['run_%d_fold_%d' % (run_id, fold_id)]['te_index']
+    print('number of tr: %d number of te: %d' % (len(tr_index), len(te_index)))
+    # cross validate based on tr_index
+    list_auc_wt = np.zeros(para_spaces['conf_k_fold'])
+    list_auc_wt_bar = np.zeros(para_spaces['conf_k_fold'])
+    kf = KFold(n_splits=para_spaces['conf_k_fold'], shuffle=False)
+    for ind, (sub_tr_ind, sub_te_ind) in enumerate(kf.split(np.zeros(shape=(len(tr_index), 1)))):
+        re = get_sub_data_by_indices(data, tr_index, sub_tr_ind)
+        sub_x_tr_values, sub_x_tr_indices, sub_x_tr_positions, sub_x_tr_len_list = re
+        sub_y_tr = data['y_tr'][tr_index[sub_tr_ind]]
+        re = c_algo_spam_sparse(np.asarray(sub_x_tr_values, dtype=float),
+                                np.asarray(sub_x_tr_indices, dtype=np.int32),
+                                np.asarray(sub_x_tr_positions, dtype=np.int32),
+                                np.asarray(sub_x_tr_len_list, dtype=np.int32),
+                                np.asarray(sub_y_tr, dtype=float),
+                                len(sub_y_tr),
+                                data['p'],
+                                para_spaces['para_xi'],
+                                para_spaces['para_l1_reg'],
+                                para_spaces['para_beta'],
+                                para_spaces['para_reg_opt'],
+                                para_spaces['para_num_passes'],
+                                para_spaces['para_step_len'],
+                                para_spaces['para_is_sparse'],
+                                para_spaces['para_verbose'])
+        wt = np.asarray(re[0])
+        wt_bar = np.asarray(re[1])
+        re = get_sub_data_by_indices(data, tr_index, sub_te_ind)
+        sub_x_te_values, sub_x_te_indices, sub_x_te_positions, sub_x_te_len_list = re
+        sub_y_te = data['y_tr'][tr_index[sub_te_ind]]
+        y_pred_wt, y_pred_wt_bar = np.zeros_like(sub_y_te), np.zeros_like(sub_y_te)
+        for i in range(len(sub_te_ind)):
+            cur_posi = sub_x_te_positions[i]
+            cur_len = sub_x_te_len_list[i]
+            cur_x = sub_x_te_values[cur_posi:cur_posi + cur_len]
+            cur_ind = sub_x_te_indices[cur_posi:cur_posi + cur_len]
+            y_pred_wt[i] = np.sum([cur_x[_] * wt[cur_ind[_]] for _ in range(cur_len)])
+            y_pred_wt_bar[i] = np.sum([cur_x[_] * wt_bar[cur_ind[_]] for _ in range(cur_len)])
+        list_auc_wt[ind] = roc_auc_score(y_true=sub_y_te, y_score=y_pred_wt)
+        list_auc_wt_bar[ind] = roc_auc_score(y_true=sub_y_te, y_score=y_pred_wt_bar)
+    print('run_id, fold_id, para_xi, para_beta: ', run_id, fold_id, para_xi, para_beta)
+    print('list_auc_wt:', list_auc_wt)
+    print('list_auc_wt_bar:', list_auc_wt_bar)
+    run_time = time.time() - s_time
+    return {'algo_para_name_list': ['run_id', 'fold_id', 'para_xi', 'para_beta',
+                                    'num_passes', 'num_runs', 'k_fold'],
+            'algo_para': para, 'para_spaces': para_spaces,
+            'list_auc_wt': list_auc_wt,
+            'list_auc_wt_bar': list_auc_wt_bar,
+            'run_time': run_time}
+
+
+def run_single_ms_sht_am(para):
+    """
+    Model selection of SPAM-l2
+    :return:
+    """
+    run_id, fold_id, para_xi, para_beta, num_passes, num_runs, k_fold = para
+    s_time = time.time()
+    data = load_dataset(root_path=root_path, name='realsim')
+    para_spaces = {'conf_num_runs': num_runs,
+                   'conf_k_fold': k_fold,
+                   'para_num_passes': num_passes,
+                   'para_beta': para_beta,
+                   'para_l1_reg': 0.0,  # no l1 regularization needed.
+                   'para_xi': para_xi,
+                   'para_fold_id': fold_id,
+                   'para_run_id': run_id,
+                   'para_verbose': 0,
+                   'para_is_sparse': 1,
+                   'para_step_len': 400000000,
+                   'para_reg_opt': 1,
+                   'data_id': 13,
+                   'data_name': '13_realsim'}
+    tr_index = data['run_%d_fold_%d' % (run_id, fold_id)]['tr_index']
+    te_index = data['run_%d_fold_%d' % (run_id, fold_id)]['te_index']
+    print('number of tr: %d number of te: %d' % (len(tr_index), len(te_index)))
+    # cross validate based on tr_index
+    list_auc_wt = np.zeros(para_spaces['conf_k_fold'])
+    list_auc_wt_bar = np.zeros(para_spaces['conf_k_fold'])
+    kf = KFold(n_splits=para_spaces['conf_k_fold'], shuffle=False)
+    for ind, (sub_tr_ind, sub_te_ind) in enumerate(kf.split(np.zeros(shape=(len(tr_index), 1)))):
+        re = get_sub_data_by_indices(data, tr_index, sub_tr_ind)
+        sub_x_tr_values, sub_x_tr_indices, sub_x_tr_positions, sub_x_tr_len_list = re
+        sub_y_tr = data['y_tr'][tr_index[sub_tr_ind]]
+        re = c_algo_spam_sparse(np.asarray(sub_x_tr_values, dtype=float),
+                                np.asarray(sub_x_tr_indices, dtype=np.int32),
+                                np.asarray(sub_x_tr_positions, dtype=np.int32),
+                                np.asarray(sub_x_tr_len_list, dtype=np.int32),
+                                np.asarray(sub_y_tr, dtype=float),
+                                len(sub_y_tr),
+                                data['p'],
+                                para_spaces['para_xi'],
+                                para_spaces['para_l1_reg'],
+                                para_spaces['para_beta'],
+                                para_spaces['para_reg_opt'],
+                                para_spaces['para_num_passes'],
+                                para_spaces['para_step_len'],
+                                para_spaces['para_is_sparse'],
+                                para_spaces['para_verbose'])
+        wt = np.asarray(re[0])
+        wt_bar = np.asarray(re[1])
+        re = get_sub_data_by_indices(data, tr_index, sub_te_ind)
+        sub_x_te_values, sub_x_te_indices, sub_x_te_positions, sub_x_te_len_list = re
+        sub_y_te = data['y_tr'][tr_index[sub_te_ind]]
+        y_pred_wt, y_pred_wt_bar = np.zeros_like(sub_y_te), np.zeros_like(sub_y_te)
+        for i in range(len(sub_te_ind)):
+            cur_posi = sub_x_te_positions[i]
+            cur_len = sub_x_te_len_list[i]
+            cur_x = sub_x_te_values[cur_posi:cur_posi + cur_len]
+            cur_ind = sub_x_te_indices[cur_posi:cur_posi + cur_len]
+            y_pred_wt[i] = np.sum([cur_x[_] * wt[cur_ind[_]] for _ in range(cur_len)])
+            y_pred_wt_bar[i] = np.sum([cur_x[_] * wt_bar[cur_ind[_]] for _ in range(cur_len)])
+        list_auc_wt[ind] = roc_auc_score(y_true=sub_y_te, y_score=y_pred_wt)
+        list_auc_wt_bar[ind] = roc_auc_score(y_true=sub_y_te, y_score=y_pred_wt_bar)
+    print('run_id, fold_id, para_xi, para_beta: ', run_id, fold_id, para_xi, para_beta)
+    print('list_auc_wt:', list_auc_wt)
+    print('list_auc_wt_bar:', list_auc_wt_bar)
+    run_time = time.time() - s_time
+    return {'algo_para_name_list': ['run_id', 'fold_id', 'para_xi', 'para_beta',
+                                    'num_passes', 'num_runs', 'k_fold'],
+            'algo_para': para, 'para_spaces': para_spaces,
+            'list_auc_wt': list_auc_wt,
+            'list_auc_wt_bar': list_auc_wt_bar,
+            'run_time': run_time}
+
+
+def run_ms_sht_am():
     if 'SLURM_ARRAY_TASK_ID' in os.environ:
         task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
     else:
@@ -193,7 +343,7 @@ def run_model_selection_sht_am():
     for run_id, fold_id in product(range(num_runs), range(k_fold)):
         for num_passes in [global_passes]:
             for para_xi in 10. ** np.arange(-7, -2., 0.5, dtype=float):
-                for para_beta in 10. ** np.arange(-6, 1, 1, dtype=float):
+                for para_beta in 10. ** np.arange(-5, 1, 1, dtype=float):
                     para_row = (run_id, fold_id, para_xi, para_beta, num_passes, num_runs, k_fold)
                     all_para_space.append(para_row)
     # only run sub-tasks for parallel
@@ -203,13 +353,13 @@ def run_model_selection_sht_am():
     list_tasks = all_para_space[task_start:task_end]
     list_results = []
     for task_para in list_tasks:
-        result = test_single_ms_spam_l2(task_para)
+        result = run_single_ms_sht_am(task_para)
         list_results.append(result)
     file_name = 'ms_spam_l2_task_%02d_passes_%03d.pkl' % (task_id, global_passes)
     pkl.dump(list_results, open(os.path.join(root_path, file_name), 'wb'))
 
 
-def run_spam_l2_by_selected_model(id_=None, model='wt', num_passes=1):
+def run_spam_l2_by_sm(id_=None, model='wt', num_passes=1):
     """
     25 tasks to finish
     :return:
@@ -323,8 +473,8 @@ def show_graph():
 
 def run_test():
     for num_passes in [1, 5, 10, 20, 30, 40, 50]:
-        run_spam_l2_by_selected_model(id_=None, model='wt', num_passes=num_passes)
-        run_spam_l2_by_selected_model(id_=None, model='wt_bar', num_passes=num_passes)
+        run_spam_l2_by_sm(id_=None, model='wt', num_passes=num_passes)
+        run_spam_l2_by_sm(id_=None, model='wt_bar', num_passes=num_passes)
 
 
 def run_test_result():
@@ -334,7 +484,7 @@ def run_test_result():
 
 
 def main():
-    run_model_selection_sht_am()
+    run_ms_sht_am()
 
 
 if __name__ == '__main__':
