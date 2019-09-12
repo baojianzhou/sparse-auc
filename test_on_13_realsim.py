0adc29dbe0b066396c1db978809eafc74487bc16
Baojian Zhou <bzhou6@albany.edu>
2019-09-12 16:30:32 -0400

    dd

--- test_on_13_realsim.py
+++ test_on_13_realsim.py
@@ -308,7 +308,8 @@ def run_spam_l2_by_sm(id_=None, model='wt', num_passes=1):
 
     all_results, num_runs, k_fold = [], 5, 5
     for task_id in range(num_runs * k_fold):
-        f_name = root_path + 'ms_spam_l2_task_%02d_passes_%03d.pkl' % (task_id, num_passes)
+        f_name = root_path + '13_realsim/ms_spam_l2_task_%02d_passes_%03d.pkl' % \
+                 (task_id, num_passes)
         all_results.extend(pkl.load(open(f_name, 'rb')))
     # selected model
     selected_model = dict()
