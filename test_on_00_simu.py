17008a8b4005bbed7cca87529d4521d213e4497a
Baojian Zhou <bzhou6@albany.edu>
2019-09-16 19:51:28 -0400

    test slurm

--- test_on_00_simu.py
+++ test_on_00_simu.py
@@ -412,35 +412,23 @@ def run_model_selection():
         task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
     else:
         task_id = 0
-    k_fold = 5
+    k_fold, num_passes = 5, 10
     tr_list = [1000]
     mu_list = [0.3]
     posi_ratio_list = [0.5]
-    fig_list = ['fig_1', 'fig_2', 'fig_3', 'fig_4']
-    for num_tr, mu, posi_ratio in product(tr_list, mu_list, posi_ratio_list):
+    fig_list = ['fig_1']
+    for num_tr, mu, posi_ratio, fig_i in product(tr_list, mu_list, posi_ratio_list, fig_list):
         f_name = data_path + 'data_task_%02d_tr_%03d_mu_%.1f_p-ratio_%.1f.pkl'
         data = pkl.load(open(f_name % (task_id, num_tr, mu, posi_ratio), 'rb'))
         results = dict()
-        for fig_i in fig_list:
-            item = (num_tr, mu, posi_ratio, fig_i)
-            results[item] = dict()
-            results[item]['spam_l2'] = dict()
-            results[item]['spam_l1l2'] = dict()
-            results[item]['sht_am'] = dict()
-            for num_passes in [5, 10, 15, 20]:
-                s_time = time.time()
-                re = run_spam_l2_cv(task_id, k_fold, num_passes, data[fig_i])
-                results[item]['spam_l2'][num_passes] = re
-                re = run_spam_l1l2_cv(task_id, k_fold, num_passes, data[fig_i])
-                results[item]['spam_l1l2'][num_passes] = re
-                re = run_sht_am_cv(task_id, k_fold, num_passes, data[fig_i])
-                results[item]['sht_am'][num_passes] = re
-                re = run_graph_am_cv(task_id, k_fold, num_passes, data[fig_i])
-                results[item]['graph_am'][num_passes] = re
-                exit()
-            print(time.time() - s_time)
-        f_name = os.path.join(data_path, 'ms_task_%02d_mu_%.1f_posi_ratio_%.1f.pkl' %
-                              (task_id, mu, posi_ratio))
+        item = (num_tr, mu, posi_ratio, fig_i, num_passes)
+        results[item] = dict()
+        results[item]['spam_l2'] = run_spam_l2_cv(task_id, k_fold, num_passes, data[fig_i])
+        results[item]['spam_l1l2'] = run_spam_l1l2_cv(task_id, k_fold, num_passes, data[fig_i])
+        results[item]['sht_am'] = run_sht_am_cv(task_id, k_fold, num_passes, data[fig_i])
+        results[item]['graph_am'] = run_graph_am_cv(task_id, k_fold, num_passes, data[fig_i])
+        f_name = os.path.join(data_path, 'ms_task_%02d_tr_%03d_mu_%.1f_p-ratio_%.1f_%s.pkl' %
+                              (task_id, num_tr, mu, posi_ratio, fig_i))
         pkl.dump({task_id: results}, open(f_name, 'wb'))
 
 
