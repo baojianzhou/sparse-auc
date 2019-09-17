042aaf8d2d8a3d810297049149baacd3337a5000
Baojian Zhou <bzhou6@albany.edu>
2019-09-17 08:52:13 -0400

    dd

--- algo_wrapper/auc_opt_methods.c
+++ algo_wrapper/auc_opt_methods.c
@@ -2918,7 +2918,7 @@ bool _algo_graph_am(const double *x_tr,
             for (int kk = 0; kk < p; kk++) {
                 proj_prizes[kk] = u[kk] * u[kk];
             }
-            int g = 1, sparsity_low = para_sparsity - 2, sparsity_high = para_sparsity + 2;
+            int g = 1, sparsity_low = para_sparsity, sparsity_high = para_sparsity + 2;
             int tail_max_iter = 50, verbose = 0;
             head_tail_binsearch(edges, weights, proj_prizes, p, m, g, -1, sparsity_low,
                                 sparsity_high, tail_max_iter, GWPruning, verbose, graph_stat);
