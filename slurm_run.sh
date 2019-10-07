c7906c84539b0218e51cfae921747f82b5590dba
Baojian Zhou <bzhou6@albany.edu>
2019-10-07 10:25:21 -0400

    dd

--- slurm_run.sh
+++ slurm_run.sh
@@ -4,18 +4,19 @@
 # $3: partition name
 # $4: node id
 # $5: python_file
-#SBATCH --job-name=09-opauc
+#SBATCH --job-name=09-spam-l1
 #SBATCH --output=/network/rit/lab/ceashpc/bz383376/git/sparse-auc/logs/array_%A_%02a.out
-#SBATCH --array=00-24
-#SBATCH --nodelist=uagc19-05
+#SBATCH --array=00-00
+#SBATCH --nodelist=ceashpc-01
 #SBATCH --time=96:00:00
-#SBATCH --partition=batch
-#SBATCH --mem=2G
+#SBATCH --partition=ceashpc
+#SBATCH --mem=8G
 #SBATCH --ntasks=1
 # %A_%a notation is filled in with the master job id (%A) and the array task id (%a)
 # the environment variable SLURM_ARRAY_TASK_ID contains
 # the index corresponding to the current job step
 # /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_13_realsim.py 
 # /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_00_simu.py 
-/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_09_sector.py 
+# /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_09_sector.py 
+/network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_high_dim_data.py 
 # /network/rit/lab/ceashpc/bz383376/opt/env-python2.7.14/bin/python test_on_16_bc.py graph_am 
