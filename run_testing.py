b8aba0b724cd4da4ae07dd4941fa17653dfa15fb
Baojian Zhou <bzhou6@albany.edu>
2019-10-14 16:06:11 -0400

    add lab-03

--- run_testing.py
+++ run_testing.py
@@ -113,7 +113,7 @@ def get_model(data_name, method, run_id, fold_id):
 
 
 def run_all_methods():
-    task_id = int(os.environ['SLURM_ARRAY_TASK_ID']) if 'SLURM_ARRAY_TASK_ID' in os.environ else 0
+    task_id = int(os.environ['SLURM_ARRAY_TASK_ID']) if 'SLURM_ARRAY_TASK_ID' in os.environ else 4
     run_id, fold_id, k_fold, passes, step_len = task_id / 5, task_id % 5, 5, 20, 50
     data_name = sys.argv[1]
     f_name = os.path.join(data_path, '%s/data_run_%d.pkl' % (data_name, run_id))
@@ -208,7 +208,7 @@ def main(data_name, task_id):
     # -----------------------
     method = 'sht_am'
     para_s, para_b, para_c = get_model(data_name, method, run_id, fold_id)
-    para_c = 55.
+    para_c = 32.
     wt, wt_bar, auc, rts = c_algo_sht_am_sparse(
         x_vals, x_inds, x_poss, x_lens, y_tr,
         data['p'], para_s, para_b, para_c, 0.0, passes, step_len, 0)
@@ -220,10 +220,10 @@ def main(data_name, task_id):
 
 def test_1():
     x = []
-    for _ in range(1):
-        x.append(main(data_name='02_pcmacs', task_id=_))
-    print(np.mean(x))
+    for _ in range(14):
+        x.append(main(data_name='12_news20', task_id=_))
+    print(np.mean(x), np.std(x))
 
 
 if __name__ == '__main__':
-    run_all_methods()
+    test_1()
