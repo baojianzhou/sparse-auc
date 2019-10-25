3e8e6357c0c685d22028e5eeb4f0fdcd7d12d08b
Baojian Zhou <bzhou6@albany.edu>
2019-10-25 09:43:33 -0400

    test slurm

--- cv_low_dim_bc.py
+++ cv_low_dim_bc.py
@@ -573,24 +573,35 @@ def cv_fsauc(method_name, k_fold, task_id, num_passes, step_len, data):
 
 def cv_solam(method_name, k_fold, task_id, num_passes, step_len, data):
     results = dict()
-    list_xi = np.arange(1, 101, 9, dtype=float)
-    list_r = 10 ** np.arange(-1, 6, 1, dtype=float)
     for fold_id in range(k_fold):
         results[(task_id, fold_id)] = dict()
         tr_index = data['run_%d_fold_%d' % (task_id, fold_id)]['tr_index']
         te_index = data['run_%d_fold_%d' % (task_id, fold_id)]['te_index']
         best_auc = None
-        for para_xi, para_r in product(list_xi, list_r):
-            wt, wt_bar, auc, rts = c_algo_solam(
-                np.asarray(data['data_x_tr'][tr_index], dtype=float),
-                np.asarray(data['data_y_tr'][tr_index], dtype=float),
-                para_xi, para_r, num_passes, step_len, 0)
-            auc_wt, auc_wt_bar = pred(wt, wt_bar, te_index, data)
-            print(fold_id, para_xi, para_r, auc_wt, auc_wt_bar)
-            if best_auc is None or best_auc['auc_wt'] < auc_wt:
-                best_auc = {'auc_wt': auc_wt, 'auc_wt_bar': auc_wt_bar,
-                            'auc': auc, 'rts': rts, 'para_xi': para_xi, 'para_r': para_r}
-        results[(task_id, fold_id)][method_name] = best_auc
+        fold = KFold(n_splits=k_fold, shuffle=False)
+        list_xi = np.arange(1, 101, 9, dtype=float)
+        list_r = 10 ** np.arange(-1, 6, 1, dtype=float)
+        for para_c, para_r in product(list_xi, list_r):
+            s_time = time.time()
+            auc_arr = np.zeros(k_fold)
+            for ind, (sub_tr_ind, sub_te_ind) in enumerate(fold.split(tr_index)):
+                wt, wt_bar, auc, rts = c_algo_solam(
+                    np.asarray(data['data_x_tr'][tr_index], dtype=float),
+                    np.asarray(data['data_y_tr'][tr_index], dtype=float),
+                    para_c, para_r, num_passes, step_len, 0)
+                auc_wt, auc_wt_bar = pred(wt, wt_bar, tr_index[sub_te_ind], data)
+                auc_arr[ind] = auc_wt
+            print(fold_id, para_c, para_r, 'auc: %.5f run_time: %.5f' %
+                  (float(np.mean(auc_arr)), time.time() - s_time))
+            if best_auc is None or best_auc['auc'] < np.mean(auc_arr):
+                best_auc = {'auc': np.mean(auc_arr), 'para_c': para_c, 'para_r': para_r}
+        wt, wt_bar, auc, rts = c_algo_solam(
+            np.asarray(data['data_x_tr'][tr_index], dtype=float),
+            np.asarray(data['data_y_tr'][tr_index], dtype=float),
+            best_auc['para_c'], best_auc['para_r'], num_passes, step_len, 0)
+        auc_wt, auc_wt_bar = pred(wt, wt_bar, te_index, data)
+        results[(task_id, fold_id)][method_name] = {
+            'auc': auc_wt, 'rts': rts, 'ms': best_auc, 'wt': wt}
     return results
 
 
