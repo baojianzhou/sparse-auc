207a9d9432a155c240c294751392c2209fe14ab5
Baojian Zhou <bzhou6@albany.edu>
2019-12-23 22:00:39 -0500

    dd

--- cv_simu_01.py
+++ cv_simu_01.py
@@ -81,6 +81,7 @@ def cv_spam_l1(para):
     print('AUC-wt-bar: ' + ' '.join(['%.4f' % auc_wt_bar[_]['auc'] for _ in auc_wt_bar]))
     print('nonzeros-wt: ' + ' '.join(['%.4f' % auc_wt[_]['num_nonzeros'] for _ in auc_wt]))
     print('nonzeros-wt-bar: ' + ' '.join(['%.4f' % auc_wt_bar[_]['num_nonzeros'] for _ in auc_wt_bar]))
+    sys.stdout.flush()
     return para, auc_wt, auc_wt_bar, cv_wt_results
 
 
